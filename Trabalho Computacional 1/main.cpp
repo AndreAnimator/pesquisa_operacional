f98166fd260c975b0c99dcb3092bb77c6195ecd4
yorranan <yorranan.costa@outlook.com>
2024-05-11 16:49:41 -0300

    created monolithic code

--- Trabalho Computacional 1/main.cpp
+++ Trabalho Computacional 1/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <clocale>
-#include "fileOperations.cpp"
-#include "matrixOperations.cpp"
+#include <fstream>
+#include <sstream>
 
 using namespace std;
+int getMatrix(vector<vector<double>>& matrix);
+double calcDeterminant(std::vector<std::vector<double>> matrix, int order);
+void switchLines(vector<vector<double>>& matrix, int x_line, int y_line);
+void printMatrix(vector<vector<double>> matrix);
 
 int main() {
 
@@ -24,3 +28,74 @@ int main() {
     // Exemplo: matrix[0][0] retorna o primeiro elemento da matriz
     return 0;
 }
+
+int getMatrix(vector<vector<double>>& matrix)
+{
+    // Inicilizando duas matrizes vazias
+    string line;
+    ifstream file("matriz.txt");
+    if (!file.is_open()) {
+        cerr << "Erro ao abrir o arquivo!" << endl;
+        return 1;
+    }
+
+    // Leitura do arquivo linha a linha
+    while (getline(file, line)) {
+        vector<double> matrix_line;
+        stringstream ss(line);
+        double valor;
+
+        // Tratando todos os elementos como das linhas como double
+        while (ss >> valor) {
+            matrix_line.push_back(valor);
+        }
+        matrix.push_back(matrix_line);
+    }
+    file.close();
+    return 0;
+}
+
+double calcDeterminant(std::vector<std::vector<double>> matrix, int order)
+{
+    double multiply;
+    double determinant = 1;
+
+    for (int i = 0; i < order; i++) {
+        for (int j = 0; j < order; j++) {
+            multiply = matrix[j][i] / matrix[i][i];
+            for (int k = 0; k < order; k++) {
+                if (i != j)
+                    matrix[j][k] = matrix[j][k] - matrix[i][k] * multiply;
+            }
+        }
+    }
+
+    for (int i = 0; i < order; i++) {
+        determinant *= matrix[i][i];
+    }
+
+    return determinant;
+}
+
+void switchLines(vector<vector<double>>& matrix, int x_line, int y_line)
+{
+    double aux;
+
+    for (int i = 0; i < matrix.size(); i++)
+    {
+        aux = matrix[x_line][i];
+		matrix[x_line][i] = matrix[y_line][i];
+		matrix[y_line][i] = aux; 
+    }
+}
+
+void printMatrix(vector<vector<double>> matrix)
+{
+    for (int i = 0; i < matrix.size(); i++) {
+        for (int j = 0; j < matrix[i].size(); j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
