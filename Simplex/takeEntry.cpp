d2626c43f75dd0358289152d269badecf81f477f
yorranan <yorranan.costa@outlook.com>
2024-05-27 11:30:48 -0300

    quase

--- Simplex/takeEntry.cpp
+++ Simplex/takeEntry.cpp
@@ -13,6 +13,11 @@ typedef struct
     vector<string> expressions;
 } FileContent;
 
+bool isLogicalOperator(string str)
+{
+    return str == ">=" || str == "<=" || str == "=";
+}
+
 vector<string> split(const string &s, char delimiter)
 {
     vector<string> tokens;
@@ -31,48 +36,75 @@ void parseString(const string &input, double &value, int &index, bool debug = fa
     value = 1.0;
     index = 0;
 
-    // Verifica se a string é vazia
+    // Check if the string is empty
     if (input.empty())
     {
+        cerr << "Input string is empty." << endl;
         return;
     }
 
     size_t pos = 0;
     bool isNegative = false;
 
-    // Verifica se o termo é negativo
+    // Check if the term is negative
     if (input[pos] == '+' || input[pos] == '-')
     {
         isNegative = (input[pos] == '-');
         pos++;
     }
 
-    // Acha a posição de 'x'
+    // Find the position of 'x'
     size_t xPos = input.find('x', pos);
     if (xPos == string::npos)
     {
-        cerr << "Invalid format: 'x' not found" << endl;
+        cerr << "Invalid format: 'x' not found in '" << input << "'" << endl;
         return;
     }
 
-    // Extrai o valor
+    // Extract the value
     if (xPos > pos)
     {
         string valueStr = input.substr(pos, xPos - pos);
-        value = stof(valueStr);
+        try
+        {
+            value = stof(valueStr);
+        }
+        catch (const invalid_argument &e)
+        {
+            cerr << "Invalid_argument error: Unable to convert '" << valueStr << "' to float in input: " << input << endl;
+            return;
+        }
+        catch (const out_of_range &e)
+        {
+            cerr << "Out_of_range error: '" << valueStr << "' is out of range in input: " << input << endl;
+            return;
+        }
     }
 
-    // Aplica o sinal negativo
+    // Apply the negative sign if needed
     if (isNegative)
     {
         value = -value;
     }
 
-    // Extrai o indice
+    // Extract the index
     string indexStr = input.substr(xPos + 1);
     if (!indexStr.empty())
     {
-        index = stoi(indexStr);
+        try
+        {
+            index = stoi(indexStr);
+        }
+        catch (const invalid_argument &e)
+        {
+            cerr << "Invalid_argument error: Unable to convert '" << indexStr << "' to int in input: " << input << endl;
+            return;
+        }
+        catch (const out_of_range &e)
+        {
+            cerr << "Out_of_range error: '" << indexStr << "' is out of range in input: " << input << endl;
+            return;
+        }
     }
 
     if (debug)
@@ -82,6 +114,7 @@ void parseString(const string &input, double &value, int &index, bool debug = fa
     }
 }
 
+
 vector<string> tokenizeExpression(const string &expression)
 {
     vector<string> tokens;
@@ -90,22 +123,25 @@ vector<string> tokenizeExpression(const string &expression)
 
     while (getline(iss, token, ' '))
     {
-        // Remove espaços em branco
+        // Remove spaces and ensure token is not empty
         token.erase(remove(token.begin(), token.end(), ' '), token.end());
-
-        if (token[0] == 'x')
+        if (!token.empty())
         {
-            tokens.push_back("+1.0" + token);
-        }
-        else if (token[0] == '+' || token[0] == '-')
-        {
-            tokens.push_back(token);
-        }
-        else
-        {
-            tokens.push_back("+" + token);
+            if (token[0] == 'x')
+            {
+                tokens.push_back("+1.0" + token);
+            }
+            else if (token[0] == '+' || token[0] == '-')
+            {
+                tokens.push_back(token);
+            }
+            else
+            {
+                tokens.push_back("+" + token);
+            }
         }
     }
+
     vector<string> newTokens;
     for (int i = 0; i < tokens.size(); i++)
     {
@@ -117,27 +153,34 @@ vector<string> tokenizeExpression(const string &expression)
     return newTokens;
 }
 
-vector<double> catchExpression(string str, int tam = 0, bool isExpression = true)
+
+vector<double> catchExpression(string str, int& max_index, bool isExpression = true, bool debug = false, int tam = 0)
 {
     int i = 0;
-    size_t equalPos = str.find('=');
-    string substr = str.substr(equalPos + 1, str.size());
-    cout << substr << endl;
+    size_t limitPosition;
+    string substr = "";
+    if (!isExpression)
+    {
+        limitPosition = str.find('=');
+        substr = str.substr(limitPosition + 1);
+    }
+    limitPosition = str.find(isLogicalOperator(str));
+    substr = str.substr(0, limitPosition);
+    if (debug) cout << "Substr: " << substr << endl;
+
     vector<string> tokens = tokenizeExpression(substr);
     int index;
     double value;
     vector<pair<double, int>> values; // valores dos coeficientes
-    for (int i = 0; i < tokens.size(); i++)
+    for (i = 0; i < tokens.size(); i++)
     {
-        parseString(tokens[i], value, index);
+        parseString(tokens[i], value, index, debug);
+        if (index > max_index) max_index = index;
         values.push_back(make_pair(value, index));
     }
-    if (tam == 0)
-    {
-        tam = values.size();
-    }
+    if (tam == 0) tam = values.size();
     vector<double> result(tam, 0.0);
-    for (int i = 0; i < values.size(); i++)
+    for (i = 0; i < values.size(); i++)
     {
         result[values[i].second - 1] = values[i].first;
     }
@@ -166,17 +209,25 @@ FileContent readFile()
 int main()
 {
     FileContent fileContent = readFile();
-    vector<vector<double>> expressions;
-    vector<double> goal = catchExpression(fileContent.goal,,false);
-    // Tem que tratar para ignorar o sinal de maior e menor igual no final
-    // for (int i = 0; i < fileContent.expressions.size(); i++)
-    // {
-    //     expressions.push_back(catchExpression(fileContent.expressions[i], goal.size()));
-    // }
-    
+    int max_index = 0;
+    vector<double> goal = catchExpression(fileContent.goal, max_index, false, true, 0);
+    vector<vector<double>> expressions(max_index, vector<double>(0));
+    for (int i = 0; i < fileContent.expressions.size(); i++)
+    {
+        expressions.push_back(catchExpression(fileContent.expressions[i], max_index, true, true, max_index));
+    }
     for (int i = 0; i < goal.size(); i++)
     {
         cout << goal[i] << " ";
+    }
+    printf("\n");
+    for (int i = 0; i < expressions.size(); i++)
+    {
+        for (int j = 0; j < expressions[i].size(); j++)
+        {
+            cout << expressions[i][j] << " ";
+        }
+        printf("\n");
     }
     return 0;
 }
