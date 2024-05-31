021044fab4ea17f5ed4ad2e1ccc3c93b4c2f8074
yorranan <yorranan.costa@outlook.com>
2024-05-31 15:35:42 -0300

    necessita-se solucionar problema de apenas um sinal de inequação

--- Simplex/takeEntry.cpp
+++ Simplex/takeEntry.cpp
@@ -4,22 +4,26 @@
 #include <vector>
 #include <sstream>
 #include <algorithm>
+#include <iomanip>
+
+
+#define POSITIVE_SIGNAL true
+#define NEGATIVE_SIGNAL false
 
 using namespace std;
+
+bool isMaximization = false;
+bool haveOneBiggerThanZero = false;
+vector<double> goal;
 
 typedef struct
 {
     string goal;
     vector<string> expressions;
-} FileContent;
-
-bool isLogicalOperator(string str)
-{
-    return str == ">=" || str == "<=" || str == "=";
-}
+}FileContent;
 
 vector<string> split(const string &s, char delimiter)
-{
+{ 
     vector<string> tokens;
     stringstream ss(s);
     string token;
@@ -30,143 +34,160 @@ vector<string> split(const string &s, char delimiter)
     return tokens;
 }
 
-void parseString(const string &input, double &value, int &index, bool debug = false)
-{
-    // Default values
+void tokenParser(string &input, int *index, double &value) {
     value = 1.0;
-    index = 0;
+    *index = 0;
 
-    // Check if the string is empty
     if (input.empty())
     {
-        cerr << "Input string is empty." << endl;
+        cerr << "A string da entrada está vazia!" << endl;
         return;
     }
 
     size_t pos = 0;
     bool isNegative = false;
-
-    // Check if the term is negative
-    if (input[pos] == '+' || input[pos] == '-')
+    if (input[0] == '-')
     {
-        isNegative = (input[pos] == '-');
+        isNegative = true;
+    } 
+    else if (input[0] != '+')
+    {
+        string aux = "+";
+        aux.append(input);
+        input = aux;
+        pos++;
+    }
+    else
+    {
         pos++;
     }
 
-    // Find the position of 'x'
-    size_t xPos = input.find('x', pos);
+    size_t xPos = input.find('x');
+    string valueStr = input.substr(pos, xPos - pos);
+    if (valueStr.empty())
+    {
+        value = 1.0;
+    }
+    else if (valueStr == "-")
+    {
+        value = -1.0;
+    }
+    else
+    {
+        value = stod(valueStr);
+    }
+
+    int aux_index; 
+    *index = stoi(input.substr(xPos + 1));
     if (xPos == string::npos)
     {
-        cerr << "Invalid format: 'x' not found in '" << input << "'" << endl;
+        cerr << "Formato Inválido: 'x' não encontrado em " << input << endl;
         return;
     }
-
-    // Extract the value
-    if (xPos > pos)
-    {
-        string valueStr = input.substr(pos, xPos - pos);
-        try
-        {
-            value = stof(valueStr);
-        }
-        catch (const invalid_argument &e)
-        {
-            cerr << "Invalid_argument error: Unable to convert '" << valueStr << "' to float in input: " << input << endl;
-            return;
-        }
-        catch (const out_of_range &e)
-        {
-            cerr << "Out_of_range error: '" << valueStr << "' is out of range in input: " << input << endl;
-            return;
-        }
-    }
-
-    // Apply the negative sign if needed
-    if (isNegative)
-    {
-        value = -value;
-    }
-
-    // Extract the index
-    string indexStr = input.substr(xPos + 1);
-    if (!indexStr.empty())
-    {
-        try
-        {
-            index = stoi(indexStr);
-        }
-        catch (const invalid_argument &e)
-        {
-            cerr << "Invalid_argument error: Unable to convert '" << indexStr << "' to int in input: " << input << endl;
-            return;
-        }
-        catch (const out_of_range &e)
-        {
-            cerr << "Out_of_range error: '" << indexStr << "' is out of range in input: " << input << endl;
-            return;
-        }
-    }
-
-    if (debug)
-    {
-        // Output for debugging
-        cout << "Input: " << input << ", Value: " << value << ", Index: " << index << endl;
-    }
 }
 
-
-vector<string> tokenizeExpression(const string &expression)
+vector<string> tokenizeExpression(string expression)
 {
+    int i = 0;
+    string newExpression = "";
+    while (expression[i] != '\0')
+    {
+        if (expression[i] == '+' && i > 0)
+        {
+            newExpression += ";+";
+        }
+        else if (expression[i] == '-' && i > 0)
+        {
+            newExpression += ";-";
+        }
+        else if (expression[i] != ' ')
+        {
+            newExpression += expression[i];
+        }
+        i++;
+    }
+    newExpression += ";";
+
     vector<string> tokens;
-    istringstream iss(expression);
-    string token;
-
-    while (getline(iss, token, ' '))
-    {
-        // Remove spaces and ensure token is not empty
-        token.erase(remove(token.begin(), token.end(), ' '), token.end());
-        if (!token.empty())
-        {
-            if (token[0] == 'x')
-            {
-                tokens.push_back("+1.0" + token);
-            }
-            else if (token[0] == '+' || token[0] == '-')
-            {
-                tokens.push_back(token);
-            }
-            else
-            {
-                tokens.push_back("+" + token);
-            }
-        }
-    }
-
-    vector<string> newTokens;
-    for (int i = 0; i < tokens.size(); i++)
-    {
-        if (tokens[i].size() > 2)
-        {
-            newTokens.push_back(tokens[i]);
-        }
-    }
-    return newTokens;
+    if (newExpression != "")
+        tokens = split(newExpression, ';');
+    return tokens;
 }
 
-
-vector<double> catchExpression(string str, int& max_index, bool isExpression = true, bool debug = false, int tam = 0)
+vector<double> parser(string str, int &max_index, vector<pair<double, bool>> &slack_value, bool is_expression = true)
 {
     int i = 0;
     size_t limitPosition;
-    string substr = "";
-    if (!isExpression)
+    string substr = "", b_value = "";
+    bool haveInequantionSignal = false;
+    bool oneSimbol = false;
+    /**
+     * Verifica se a expressão é uma restrição ou a função objetivo
+     * Se for uma restrição, a expressão é do tipo: Max Z = 2x1 + 3x2 + 4x3 <= 5
+     * Se for a função objetivo, a expressão é do tipo: 2x1 + 3x2 + 4x3
+     * string::npos é o valor retornado quando a substring não é encontrada, ele funciona como um sentinela
+     * 
+ */
+    if (str.find(">=") != string::npos || str.find(">") != string::npos)
     {
-        limitPosition = str.find('=');
+        limitPosition = str.find(">");
+        limitPosition = str.find(">=");
+        haveOneBiggerThanZero = true;
+        haveInequantionSignal = true;
+    }
+    else if (str.find("<=") != string::npos || str.find("<") != string::npos)
+    {
+        limitPosition = str.find("<");
+        limitPosition = str.find("<=");
+        haveInequantionSignal = true;
+    }
+    else if (str.find("=") != string::npos)
+    {
+        if (is_expression)
+        {
+            haveOneBiggerThanZero = true;
+        }
+        limitPosition = str.find("=");
+        if (!is_expression)
+        {
+            string aux = str.substr(0, str.size() - limitPosition - 1);
+            if (aux.find("max") != string::npos || aux.find("Max") != string::npos || aux.find("Maximização") != string::npos || aux.find("Maximize") != string::npos)
+            {
+                isMaximization = true;
+            }
+            else if (aux.find("min") != string::npos || aux.find("Min") != string::npos || aux.find("Minimização") != string::npos || aux.find("Minimize") != string::npos)
+            {
+                isMaximization = false;
+            }
+            else {
+                cerr << "Formato Inválido: limit: " << limitPosition <<" Nenhuma palavra definindo operação encontrada em " << str.substr(0, limitPosition - 1) << endl;
+                return vector<double>();
+            }
+        }
+    }
+    else
+    {
+        cerr << "Formato Inválido: Nenhum sinal de limite encontrado em " << str << endl;
+        return vector<double>();
+    }
+    
+    if (haveInequantionSignal)
+    {
+        b_value = str.substr(limitPosition + 2);
+        if (str.find(">=") != string::npos)
+            slack_value.push_back(make_pair(stod(b_value), POSITIVE_SIGNAL));
+        else
+            slack_value.push_back(make_pair(stod(b_value), NEGATIVE_SIGNAL));
+    }
+
+    if (!is_expression)
+    {
         substr = str.substr(limitPosition + 1);
     }
-    limitPosition = str.find(isLogicalOperator(str));
-    substr = str.substr(0, limitPosition);
-    if (debug) cout << "Substr: " << substr << endl;
+    else
+    {
+        substr = str.substr(0, limitPosition);
+    }
 
     vector<string> tokens = tokenizeExpression(substr);
     int index;
@@ -174,16 +195,18 @@ vector<double> catchExpression(string str, int& max_index, bool isExpression = t
     vector<pair<double, int>> values; // valores dos coeficientes
     for (i = 0; i < tokens.size(); i++)
     {
-        parseString(tokens[i], value, index, debug);
-        if (index > max_index) max_index = index;
+        tokenParser(tokens[i], &index, value);
         values.push_back(make_pair(value, index));
+        if (index > max_index)
+            max_index = index;
     }
-    if (tam == 0) tam = values.size();
-    vector<double> result(tam, 0.0);
+
+    vector<double> result(max_index, 0.0);
     for (i = 0; i < values.size(); i++)
     {
         result[values[i].second - 1] = values[i].first;
     }
+
     return result;
 }
 
@@ -206,28 +229,86 @@ FileContent readFile()
     return fileContent;
 }
 
+void addSlackVariables(vector<vector<double>> &expressions, vector<double> &b, vector<pair<double, bool>> &slack_variables, int &max_index)
+ {
+    vector<vector<double>> aux_expression(expressions.size(), vector<double>(slack_variables.size(), 0.0));
+    int i;
+    for (i = 0; i < expressions.size(); i++)
+    {
+        if (slack_variables[i].second == POSITIVE_SIGNAL)
+        {
+            aux_expression[i][i] = -1.0;
+        }
+        else
+        {
+            aux_expression[i][i] = 1.0;
+        }
+        b.push_back(slack_variables[i].first); 
+        // Se no vetor b algum índice for menor que 0, então multiplique toda a restrição por (-1) e inverta o sentido da desigualdade   
+        expressions[i].insert(expressions[i].end(), aux_expression[i].begin(), aux_expression[i].end());
+        if (slack_variables[i].first < 0)
+        {
+            for (int j = 0; j < expressions[i].size(); j++)
+            {
+                expressions[i][j] *= -1;
+            }
+            b[i] *= -1;
+        }
+    }
+    if (isMaximization)
+    {
+        for_each(goal.begin(), goal.end(), [](double &value) {
+            value *= -1;
+        });
+        for_each(expressions.begin(), expressions.end(), [](vector<double> &expression) {
+            for_each(expression.begin(), expression.end(), [](double &value) {
+                value *= -1;
+            });
+        });
+        for_each(b.begin(), b.end(), [](double &value) {
+            value *= -1;
+        });
+    }
+}
+
+FileContent fileContent = readFile();
+
 int main()
 {
-    FileContent fileContent = readFile();
     int max_index = 0;
-    vector<double> goal = catchExpression(fileContent.goal, max_index, false, true, 0);
-    vector<vector<double>> expressions(max_index, vector<double>(0));
+    vector<pair<double, bool>> slack_variables;
+    goal = parser(fileContent.goal, max_index, slack_variables, false);
+    vector<double> b;
+    vector<vector<double>> expressions;
+    int limit_index = max_index;
     for (int i = 0; i < fileContent.expressions.size(); i++)
     {
-        expressions.push_back(catchExpression(fileContent.expressions[i], max_index, true, true, max_index));
+        expressions.push_back(parser(fileContent.expressions[i], max_index, slack_variables, true));
     }
+    addSlackVariables(expressions, b, slack_variables, limit_index);
+    cout << "Função Objetivo: \n";
     for (int i = 0; i < goal.size(); i++)
     {
-        cout << goal[i] << " ";
+        cout << fixed << setprecision(3) << goal[i] << " ";
     }
-    printf("\n");
+    cout << endl;
+    cout << "Matriz principal: \n";
     for (int i = 0; i < expressions.size(); i++)
     {
-        for (int j = 0; j < expressions[i].size(); j++)
+        if (!expressions[i].empty())
         {
-            cout << expressions[i][j] << " ";
+            for (int j = 0; j < expressions[i].size(); j++)
+            {
+                cout << fixed << setprecision(3) << expressions[i][j] << " ";
+            }
+            cout << endl;
         }
-        printf("\n");
     }
+    cout << "b: \n";
+    for (int i = 0; i < b.size(); i++)
+    {
+        cout << fixed << setprecision(3) << b[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
